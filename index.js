531a2548e0adee3f056b11b6e916d0ca92495ad8
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2022-03-31 19:14:21 +0200

    remove .env

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { Client } = require('discord.js');
 const client = new Client({ intents: 1 });
 
 client.once('ready', () => {
-    console.log('Je suis prêt');
+    console.log('Je suis prêft');
 });
 
 client.login(process.env.DISCORD_TOKEN);
