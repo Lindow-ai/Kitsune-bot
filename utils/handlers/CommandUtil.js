2c27cc7c53b83f6ce4c33d697ec2794472bb7fd4
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2022-04-04 22:08:00 +0200

    Add process and logs

--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -5,7 +5,10 @@ const pGlob = promisify(glob);
 module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdfile => {
         const cmd = require(cmdfile);
-        console.log(cmd.name, cmd);
+
+        if(!cmd.name) return console.log(`-----\nCommande non-chargée: pas de nom)\nFichier -> ${cmdfile}\n-----`);
+
         client.commands.set(cmd.name, cmd);
+        console.log(`Commande chargée: ${cmd.name}`);
     });
 };
