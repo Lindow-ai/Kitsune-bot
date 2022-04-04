c558231ddcf3dfd712d0933e0c7974eb6656cf08
Rémy Le Foll <remy.le-foll@universite-paris-saclay.fr>
2022-04-04 22:28:19 +0200

    Add slash command handler

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,7 +1,10 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
-        console.log('Je suis prêt');
+    async execute(client) {
+        console.log('Je suis prêt!');
+
+        const devGuild = await client.guilds.cache.get('930597446432718858');
+        devGuild.commands.set(client.commands.map(cmd => cmd));
     }
 }
--- utils/handlers/CommandUtil.js
+++ utils/handlers/CommandUtil.js
@@ -6,7 +6,7 @@ module.exports = async client => {
     (await pGlob(`${process.cwd()}/commands/*/*.js`)).map(async cmdfile => {
         const cmd = require(cmdfile);
 
-        if(!cmd.name) return console.log(`-----\nCommande non-chargée: pas de nom)\nFichier -> ${cmdfile}\n-----`);
+        if(!cmd.name || !cmd.description) return console.log(`-----\nCommande non-chargée: pas de nom et/ou description)\nFichier -> ${cmdfile}\n-----`);
 
         client.commands.set(cmd.name, cmd);
         console.log(`Commande chargée: ${cmd.name}`);
